1a3f3733f0ca381d767bd30a302d3dd5676b3740
Jean Faubert Dorancy <faubertdor@gmail.com>
2016-03-09 15:52:36 -0500

    Add map with marker

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ActiveRecord::Base
+  attr_accessor :map_url
   validates :name, :description, :full_address, :phone_number, presence: true
+  
+  def map_url
+    url = "https://maps.googleapis.com/maps/api/staticmap"
+    url += "?center=#{full_address}&zoom=18&size=400x400"
+    url += "&markers=color:red%7C#{full_address}&key=#{ENV['GOOGLE_MAPS']}"
+  end
 end
